7514f77554c0afaeea749286bfaa0e178d03b6ea
Edison <edisonfreire14@gmail.com>
2023-11-06 11:17:26 -0500

    files

--- Hand.cpp
+++ Hand.cpp
@@ -8,16 +8,62 @@ Hand::Hand(const Hand& other) {
     cards_ = other.cards_;
 }
 
-Hand& Hand::operator=(const Hand& other) {}
+Hand& Hand::operator=(const Hand& other) {
+    if (this != &other) {
+        cards_ = other.cards_;
+    }
+    return *this;
+}
 
-Hand::Hand(Hand&& other) {}
+Hand::Hand(Hand&& other) {
+    cards_ = std::move(other.cards_);
+}
 
-const std::deque<PointCard>& Hand::getCards() const {}
+Hand& Hand::operator=(Hand&& other) {
+    if (this != &other) {
+        cards_ = std::move(other.cards_);
+    }
+    return *this;
+}
 
-void Hand::addCard(PointCard&& card) {}
+const std::deque<PointCard>& Hand::getCards() const {
+    return cards_;
+}
 
-bool Hand::isEmpty() const {}
+void Hand::addCard(PointCard&& card) {
+    cards_.push_back(std::move(card));
+}
 
-void Hand::Reverse() {}
+bool Hand::isEmpty() const {
+    return cards_.empty();
+}
 
-int Hand::PlayCard() {}
+void Hand::Reverse() {
+    size_t left = 0;
+    size_t right = cards_.size() - 1;
+
+    while (left < right) {
+        // Swap the elements using std::swap
+        std::swap(cards_[left], cards_[right]);
+        left++;
+        right--;
+    }
+}
+
+int Hand::PlayCard() {
+        if (isEmpty()) {
+        throw std::runtime_error("Hand is empty. Cannot play a card.");
+    }
+
+    PointCard frontCard = std::move(cards_.front());
+    cards_.pop_front();
+
+    if (frontCard.isPlayable()) {
+        return std::stoi(frontCard.getInstruction());
+    } else {
+        throw std::runtime_error("Card is not playable.");
+    }
+
+    // The card is not playable, so no points are earned.
+    return 0;
+}
